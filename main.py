29c8280038fc3a67353f364d7ee4586a38ade385
rohan98866 <patankarrohan512@gmail.com>
2023-01-01 19:25:30 +0530

    main.py

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ print(__name__)
 
 input_func_name=input("Please enter the function name for training or batch pred: ")
 
+def main():
+    try:
+        if input_func_name=="training":
+            run_train=training.start_training_pipeline()
+            return run_train
+        elif input_func_name=="batch_pred":
+            input_file_path=str(input("Enter the file path: "))
+            run_batch=batch_prediction.start_batch_prediction(input_file_path=input_file_path)
+            return run_batch
+    except Exception as e:
+        raise SensorException(e,sys)
+
 if __name__ == "__main__":
-    def main():
-        try:
-            if input_func_name=="training":
-                run_train=training.start_training_pipeline()
-                return run_train
-            elif input_func_name=="batch_pred":
-                run_batch=batch_prediction.start_batch_prediction()
-                return run_batch
-        except Exception as e:
-            raise SensorException(e,sys)
+    main()
