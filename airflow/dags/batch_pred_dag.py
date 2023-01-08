f3e1ea126893d79e5ad315f0f8978a51e6dc1d3b
rohanpatankar926 <rohanpatankar95@gmail.com>
2023-01-08 17:38:57 +0530

    Apache airflow

---
+++ airflow/dags/batch_pred_dag.py
@@ -0,0 +1,49 @@
+from asyncio import tasks
+import json
+import pendulum
+import os
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+
+
+
+def download_files(**kwargs):
+    global bucket_name
+    bucket_name="sensor-data"
+    global input_dir
+    input_dir="/sensor/input_files"
+    os.makedirs(input_dir,exist_ok=True)
+    os.system(f"aws s3 sync s3://{bucket_name}/input_files /sensor/input_files")
+
+def batch_prediction(**kwargs):
+    from sensor.pipeline import batch_prediction
+    for file_name in os.listdir(input_dir):
+        batch_prediction.start_batch_prediction(input_file_path=os.path.join(input_dir,file_name))
+
+def sync_prediction_to_s3_bucket(**kwargs):
+    os.system(f"aws s3 sync /sensor/prediction s3://{bucket_name}/prediction")
+
+bash_success="echo 'Batch Prediction is completed successfully'"
+
+with DAG(
+    'sensor_training',
+    default_args={'retries': 2},
+    # [END default_args]
+    description='Sensor Fault Detection',
+    schedule_interval="@weekly",
+    start_date=pendulum.datetime(2023, 1, 9, tz="UTC"),
+    catchup=False,
+    tags=['sensor','batch_prediction'],
+) as batch_pred_dag:
+
+    download_input_files=PythonOperator(task_id="download_input_files",python_callable=download_files)
+
+    generate_prediction_files=PythonOperator(task_id="generate_prediction_files",python_callable=batch_prediction)
+
+    sync_prediction_files=PythonOperator(task_id="sync_prediction_files",python_callable=sync_prediction_to_s3_bucket)
+    
+    success=BashOperator(task_id="success",bash_command=bash_success)
+
+    download_input_files>>generate_prediction_files>>sync_prediction_files>>success
---
+++ airflow/dags/training_dag.py
@@ -0,0 +1,38 @@
+from asyncio import tasks
+import json
+import pendulum
+import os
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": pendulum.datetime(2023, 1, 9),
+    "email": ["rohanpatankar926@gmail.com"]
+}
+
+
+def training(**kwargs):
+    from sensor.pipeline import training
+    training.start_training_pipeline()
+
+def sync_data_to_s3_bucket(**kwargs):
+    bucket_name="sensor-data"
+    os.system(f"aws s3 sync /sensor/artifacts s3://{bucket_name}/artifacts")
+    os.system(f"aws s3 sync /sensor/saved_models s3://{bucket_name}/saved_models")
+
+bash_success="echo 'Training Pipeline is completed successfully'"
+
+with DAG(dag_id="training_dag",default_args=default_args,description="Sensor Training Pipeline",
+schedule_interval="@weekly",catchup=False,tags=["sensor","something"]) as dag:   
+    
+    training_pipeline=PythonOperator(task_id="training_pipeline",python_callable=training)
+
+    sync_data_2_s3_bucket=PythonOperator(task_id="sync_data_2_s3_bucket",python_callable=sync_data_to_s3_bucket)
+
+    success=BashOperator(task_id="success",bash_command=bash_success)
+
+
+    training_pipeline>>sync_data_2_s3_bucket>>success
